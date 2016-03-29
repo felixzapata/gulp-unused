d643c18d73012c2e0bdf96dc434b0dca8225ae56
Felix Zapata <fzberlinches@gmail.com>
2016-03-29 10:22:39 +0200

    refactor(gulpfile.js): remove unused task

--- gulpfile.js
+++ gulpfile.js
@@ -25,15 +25,3 @@ gulp.task('jshint', function() {
         .pipe(jshint(options));
 });
 
-gulp.task('unused', function(cb) {
-   var options = {
-        reference: 'img/',
-        directory: ['**/*.html'],
-        days: 30,
-        remove: false, // set to true to delete unused files from project
-        reportOutput:'report.txt', // set to false to disable file output
-        fail: false // set to true to make the task fail when unused files are found
-    };
-    return unused(options, cb);
-});
-
