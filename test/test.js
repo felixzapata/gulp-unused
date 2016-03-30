b8640ac6896ae2c184e89b4b8f0570b72cd772c3
Felix Zapata <fzberlinches@gmail.com>
2016-03-30 20:53:24 +0200

    test(test.js): change the use of the streams to pass the test for output report

--- test/test.js
+++ test/test.js
@@ -11,7 +11,7 @@ var assert = require('assert');
 var sassert = require('stream-assert');
 require('mocha');
 
-var fixtures = function (glob) { return path.join(__dirname, './fixtures', glob); }
+var fixtures = function (glob) { return path.join(__dirname, './.tmp', glob); }
 
 
 function fileExists(filePath) {
@@ -27,7 +27,7 @@ describe('gulp-unused', function() {
   var tmpFolder = path.join(__dirname, '.tmp');
   
   beforeEach(function(done){
-    var folder = path.join(__dirname, './fixtures');
+    var folder = path.join(__dirname, './fixtures/');
     fs.copy(folder, tmpFolder, function(err){
       done();
     });
@@ -35,10 +35,7 @@ describe('gulp-unused', function() {
 
   // We'll delete it when we're done.
   afterEach(function(done){
-    fs.remove(tmpFolder, function(error){
-      done();
-    });
-    
+    fs.remove(tmpFolder, done);
   });
   
   xit('1) should find and remove images, CSS and JavaScript not used in the project', function(){
@@ -61,13 +58,13 @@ describe('gulp-unused', function() {
     assert.equal(1, 2);
   });
   
-  it('4) create output report', function(done){
+  it('4) create output report', function(cb){
     
     var options = {
         reference: 'img/',
         reportOutput: 'report.txt'
     };
-    var expected = path.join(__dirname, './expected/result4/report.txt');
+    var expected = path.join(__dirname, './.tmp/report.txt');
     var stream = unused(options);
     var fixtureStream = fs.createReadStream(fixtures('index.html'));
     var fixtureData = '';
@@ -75,25 +72,28 @@ describe('gulp-unused', function() {
     fixtureStream.on('data', function(chunk){
       fixtureData += chunk;
     });
+   
     
     fixtureStream.on('end', function(chunk){
+      
+      stream.on('finish', function () {
+        assert.equal(fileExists(expected), true);
+        cb();
+      });
+    
       stream.write(new gutil.File({
-        base: path.join(__dirname, './fixtures/'),
+        base: path.join(__dirname, './.tmp/'),
         cwd: __dirname,
 		    path: fixtures('index.html'),
         contents: new Buffer(fixtureData)
 	    }));
+      
+      stream.end();
+      
     });
+   
     
-    stream.on('finish', function () {
-      assert.strictEqual(1, 1);
-      cb();
-    });
-
-    stream.end();
-
-    done();
-    
+ 
   });
 
 });
