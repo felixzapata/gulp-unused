263c13563402e6dd60243706bb74512fb05b6ca9
Felix Zapata <fzberlinches@gmail.com>
2016-04-05 15:38:28 +0200

    feat(mocha.js): add jshint before test task

--- tasks/mocha.js
+++ tasks/mocha.js
@@ -2,7 +2,7 @@
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 
-gulp.task('test', function () {
+gulp.task('test', ['jshint'], function () {
     return gulp.src('test.js', {cwd: './test', read: false})
         .pipe($.mocha());
 });
